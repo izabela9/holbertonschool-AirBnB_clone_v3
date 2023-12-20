ff7992b6e25710bfb08afc16484fa9ec5db62e8b
izabela9 <isabelanelaj1@gmail.com>
2023-12-20 15:18:41 +0100

    status of your api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 from flask import Blueprint
+"""
+Moudle to create Blueprint for our API
+"""
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''
+Module to create the routes
+for our api
+'''
 from flask import jsonify
 from api.v1.views import app_views
 
