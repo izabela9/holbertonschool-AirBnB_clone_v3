6af451bc2ce5e11135fc823be522a7cd196d01d8
izabela9 <isabelanelaj1@gmail.com>
2023-12-22 13:58:32 +0100

    places reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -20,9 +20,11 @@ def get_reviews(place_id=None):
 
     if place is None:
         return abort(404)
+
     if request.method == 'GET':
         reviews = [review.to_dict() for review in place.reviews]
         return jsonify(reviews)
+
     if request.method == 'POST':
         data = request.get_json(silent=True, force=True)
         if not data:
@@ -48,19 +50,23 @@ def delete_reviews(review_id=None):
     review = storage.get(Review, review_id)
     if review is None:
         return abort(404)
+
     if request.method == 'DELETE':
         storage.delete(review)
         storage.save()
         return jsonify({}), 200
+
     if request.method == 'GET':
         return jsonify(review.to_dict()), 200
+
     if request.method == 'PUT':
         data = request.get_json(force=True)
-        if not data:
+        if data is None:
             return abort(400, description="Not a JSON")
-        for k, v in data.items():
-            if k not in ['id', 'updated_at', 'created_at',
-                         'user_id', 'place_id']:
-                setattr(review, k, data[k])
+
+        for key, value in data.items():
+            if key not in ['id', 'updated_at', 'created_at',
+                           'user_id', 'place_id']:
+                setattr(review, key, value)
         storage.save()
         return jsonify(review.to_dict()), 200
