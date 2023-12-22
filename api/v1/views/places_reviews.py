461037ed60ab7323423cfc13a597c4542bfbea33
izabela9 <isabelanelaj1@gmail.com>
2023-12-22 13:31:22 +0100

    places reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -14,71 +14,53 @@ from api.v1.views import app_views
 
 @app_views.route('/places/<place_id>/reviews',
                  methods=['GET', 'POST'], strict_slashes=False)
-def handle_place_reviews(place_id):
+def get_reviews(place_id=None):
     """Handles GET and POST requests"""
     place = storage.get(Place, place_id)
 
     if place is None:
         return abort(404)
-
     if request.method == 'GET':
         reviews = [review.to_dict() for review in place.reviews]
         return jsonify(reviews)
-
     if request.method == 'POST':
         data = request.get_json(silent=True, force=True)
-
-        if data is None:
+        if not data:
             return abort(400, description="Not a JSON")
-        
         user_id = data.get("user_id", None)
         text = data.get("text", None)
-
-        if user_id not in data:
+        if user_id is None:
             return abort(400, description="Missing user_id")
-
-        if text not in data:
+        if storage.get(User, user_id) is None:
+            return abort(404)
+        if text is None:
             return abort(400, description="Missing text")
-
-        user = storage.get(User, user_id)
-
-        if user is None:
-            abort(404)
-
-        new_review = Review(place_id=place_id, **data)
+        new_review = Review(text=text, user_id=user_id, place_id=place_id)
         storage.new(new_review)
         storage.save()
-
         return jsonify(new_review.to_dict()), 201
 
 
 @app_views.route('/reviews/<review_id>',
-                 methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
-def handle_review(review_id):
+                 methods=['DELETE', 'GET', 'PUT'], strict_slashes=False)
+def delete_reviews(review_id=None):
     """Handles GET, PUT, and DELETE requests for a specific review"""
     review = storage.get(Review, review_id)
-
     if review is None:
         return abort(404)
-
-    if request.method == 'GET':
-        return jsonify(review.to_dict()), 200
-
-    if request.method == 'PUT':
-        data = request.get_json(silent=True, force=True)
-
-        if data is None:
-            return abort(400, description="Not a JSON")
-
-        ignore_keys = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
-        for key, value in data.items():
-            if key not in ignore_keys:
-                setattr(review, key, value)
-
-        storage.save()
-        return jsonify(review.to_dict()), 200
-
     if request.method == 'DELETE':
         storage.delete(review)
         storage.save()
         return jsonify({}), 200
+    if request.method == 'GET':
+        return jsonify(review.to_dict()), 200
+    if request.method == 'PUT':
+        data = request.get_json(force=True)
+        if not data:
+            return abort(400, description="Not a JSON")
+        for k, v in data.items():
+            if k not in ['id', 'updated_at', 'created_at',
+                         'user_id', 'place_id']:
+                setattr(review, k, data[k])
+        storage.save()
+        return jsonify(review.to_dict()), 200
